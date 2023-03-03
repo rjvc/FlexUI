07fcf4801fc67aa553617f314d38e862c5dbb294
rjvcastro <rjvcastro@gmail.com>
2023-03-03 17:04:57 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,74 @@
 // STICKY NAVBAR 
-// element references
-const navbar = document.querySelector(".navbar");
-const title = document.querySelector(".title");
+/*
+$(document).ready(function() {
+  // grab the initial top offset of the navigation 
+     var navbar = $('.navbar').offset().top;
+     var vpheight = $(window).height();
+     // our function that decides weather the navigation bar should have "fixed" css position or not.
+     var stickyNav = function(){
+          var scrollTop = $('.section').scrollTop(); // our current vertical position from the top
+              
+          // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+          // otherwise change it back to relative
+          if (scrollTop > vpheight) { 
+              $('.navbar').addClass('fixed-nav');
+          } else {
+              $('.navbar').removeClass('fixed-nav'); 
+          }
+      };
 
-// handler
-const handleScroll = (entries) => {
-  const spotIsVisible = entries[0].isIntersecting;
-  if (spotIsVisible) navbar.classList.remove("fixed-top") & title.classList.remove("fixed-title");
-  else navbar.classList.add("fixed-top") & title.classList.add("fixed-title");
-};
+  stickyNav();
+  // and run it again every time you scroll
+  $('.wrapper').scroll(function() {
+    stickyNav();
+  });
+});
+*/
+
+
+jQuery(function($) {
+
+  var $nav = $('.navbar');
+  var $win = $('.wrapper');
+  var winH = $(window).height()-1;   // Get the window height.
+  $('.fixed-nav-logo').addClass("hide");
+  $win.on("scroll", function () {
+      if ($('.wrapper').scrollTop() > winH ) {
+          $nav.addClass("fixed-nav");
+          $('.fixed-nav-logo').addClass("show");
+          $('.fixed-nav-logo').removeClass("hide");
+      } else {
+          $nav.removeClass("fixed-nav");
+          $('.fixed-nav-logo').addClass("hide");
+          $('.fixed-nav-logo').removeClass("show");
+      }
+  })
+  $(window).on("resize", function(){ // If the user resizes the window
+     winH = $(window).height()-1; // you'll need the new height value
+  });
+
+});
+
+//btns
+
+
+$('.wrapper').on("scroll", function () {
+  if ($('.wrapper').scrollTop() > 50 ) {
+    alert("1111");
+    const btn_top = document.querySelector('.btn-top');
+    const btn_start = document.querySelector('.btn-start');
+    btn_top.classList.add('appear');
+    btn_top.classList.remove('hidden');
+    btn_top.classList.add('show');
+    btn_start.classList.add('hidden'); 
+  } else {
+    const btn_start= document.querySelector(".btn-top");
+    (btn_start).classList.remove('show');
+    (this).classList.add('hidden'); 
+  }
+})
+
+
 
 //---------------------------------------------
 // button click -> scroll to content
@@ -19,13 +79,18 @@ function scrollToAnchor(aid){
   }, 1000 );
 }
 
-$(document).on('click', '.btn-start', function(){
-  const btn_top = document.querySelector(".btn-top");
+$(document).on('click', '.btn-start, .foreground', function(){
+  const btn_top = document.querySelector('.btn-top');
+  const btn_start = document.querySelector('.btn-start');
   scrollToAnchor('section');
-  (this).classList.add('jump');
+  btn_start.classList.add('jump');
+  btn_top.classList.add('appear');
   btn_top.classList.remove('hidden');
   btn_top.classList.add('show');
-  (this).classList.remove('jump');
+  setTimeout(() => {
+    btn_start.classList.remove('jump');
+    btn_start.classList.add('hidden'); 
+  }, "1000")
 })
 
 $(document).on('click', '.btn-top', function(){
@@ -37,5 +102,13 @@ $(document).on('click', '.btn-top', function(){
 }) 
 
 //---------------------------------------------
+// LIST MENU
 
-
+function ListMenu() {
+  var listMenu = document.getElementById("myLinks");
+  if (listMenu.style.display === "flex") {
+    listMenu.style.display = "none";
+  } else {
+    listMenu.style.display = "flex";
+  }
+}
